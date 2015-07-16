20ee28a020981d50df2efe40ab28aa50b3e84c8c
Lucas van Staden <lucas@dhemdia.com.au>
2015-07-16 23:08:53 +0800

    Fix issue pre magento 1.9 on product reviews

--- app/code/community/ProxiBlue/ReCaptcha/Model/Observer.php
+++ app/code/community/ProxiBlue/ReCaptcha/Model/Observer.php
@@ -67,15 +67,38 @@ class ProxiBlue_ReCaptcha_Model_Observer
         if ($captchaModel->isRequired()) {
             $controller = $observer->getControllerAction();
             if (!$captchaModel->isCorrect($this->_getCaptchaString($controller->getRequest(), $formId))) {
-                Mage::getSingleton('customer/session')->addError(Mage::helper('captcha')->__('Incorrect CAPTCHA.'));
-                //$controller->setFlag('', Mage_Core_Controller_Varien_Action::FLAG_NO_DISPATCH, true);
-                //$refererUrl = $this->_getRefererUrl();
-                //$controller->getResponse()->setRedirect($refererUrl);
-                //invalidate the formkey, whoich will force the controller to redirect back to referer
-                $controller->getRequest()->setParam('form_key', 'Incorrect CAPTCHA.');
+                Mage::getSingleton('core/session')->addError(Mage::helper('captcha')->__('Incorrect CAPTCHA.'));
+                $data = $controller->getRequest()->getPost();
+                Mage::getSingleton('review/session')->setFormData($data);
+                if($this->isPre19()) {
+                    $controller->setFlag('', Mage_Core_Controller_Varien_Action::FLAG_NO_DISPATCH, true);
+                    if ($redirectUrl = Mage::getSingleton('review/session')->getRedirectUrl(true)) {
+                        $controller->getResponse()->setRedirect($redirectUrl);
+                        return $this;
+                    }
+                    $controller->getResponse()->setRedirect($this->_getRefererUrl($controller));
+                } else {
+                    //invalidate the formkey, whoich will force the controller to redirect back to referer
+                    $controller->getRequest()->setParam('form_key', 'Incorrect CAPTCHA.');
+                }
             }
         }
         return $this;
+    }
+
+    /**
+     * Test if this is a pre 1.6 install
+     *
+     * @return boolean
+     */
+    public function isPre19()
+    {
+        $magentoVersion = Mage::getVersionInfo();
+        if ($magentoVersion['minor'] < 9) {
+            return true;
+        }
+
+        return false;
     }
 
     /**
@@ -93,6 +116,7 @@ class ProxiBlue_ReCaptcha_Model_Observer
 
     /**
      * Identify referer url via all accepted methods (HTTP_REFERER, regular or base64-encoded request param)
+     * Compatibility with magento < 1.9
      *
      * @return string
      */
@@ -114,5 +138,30 @@ class ProxiBlue_ReCaptcha_Model_Observer
         }
         return $refererUrl;
     }
+
+    /**
+     * Check url to be used as internal
+     * Compatibility with magento < 1.9
+     *
+     *
+     * @param   string $url
+     * @return  bool
+     */
+    protected function _isUrlInternal($url)
+    {
+        if (strpos($url, 'http') !== false) {
+            /**
+             * Url must start from base secure or base unsecure url
+             */
+            if ((strpos($url, Mage::app()->getStore()->getBaseUrl()) === 0)
+                || (strpos($url, Mage::app()->getStore()->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_LINK, true)) === 0)
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
 }
