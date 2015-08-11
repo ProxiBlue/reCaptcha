b5f38f0534817e31912a1044607055dcd2f7dba6
Lucas van Staden <sales@proxiblue.com.au>
2015-08-11 15:25:27 +0800

    Update Observer.php
    remove EE notice. fix phpdocs

--- app/code/community/ProxiBlue/ReCaptcha/Model/Observer.php
+++ app/code/community/ProxiBlue/ReCaptcha/Model/Observer.php
@@ -1,40 +1,16 @@
 <?php
-/**
- * Magento Enterprise Edition
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Magento Enterprise Edition License
- * that is bundled with this package in the file LICENSE_EE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://www.magentocommerce.com/license/enterprise-edition
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category    Mage
- * @package     Mage_Captcha
- * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://www.magentocommerce.com/license/enterprise-edition
- */
 
 /**
  * Captcha Observer
  *
  * @category    Mage
  * @package     Mage_Captcha
- * @author      Magento Core Team <core@magentocommerce.com>
+ * 
  */
 class ProxiBlue_ReCaptcha_Model_Observer
 {
     /**
-     * Check Captcha On Forgot Password Page
+     * Check Captcha On Contact Us
      *
      * @param Varien_Event_Observer $observer
      * @return Mage_Captcha_Model_Observer
@@ -58,7 +34,7 @@ class ProxiBlue_ReCaptcha_Model_Observer
     }
 
     /**
-     * Check Captcha On Forgot Password Page
+     * Check Captcha On Product Reviews
      *
      * @param Varien_Event_Observer $observer
      * @return Mage_Captcha_Model_Observer
